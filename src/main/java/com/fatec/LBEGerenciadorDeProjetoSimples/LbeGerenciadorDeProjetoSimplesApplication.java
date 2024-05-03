5cd02052567349437cda8cab58001d2c329322ae
Matheus França <matheussss6613@gmail.com>
2024-05-03 15:37:38 -0300

    add project

---
+++ src/main/java/com/fatec/LBEGerenciadorDeProjetoSimples/LbeGerenciadorDeProjetoSimplesApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.LBEGerenciadorDeProjetoSimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbeGerenciadorDeProjetoSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbeGerenciadorDeProjetoSimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/LBEGerenciadorDeProjetoSimples/LbeGerenciadorDeProjetoSimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.LBEGerenciadorDeProjetoSimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbeGerenciadorDeProjetoSimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
