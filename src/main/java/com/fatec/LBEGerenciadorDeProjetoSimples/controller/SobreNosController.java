67f2f85545004db8af520cd6f79c2b33eba07c73
Matheus França <matheussss6613@gmail.com>
2024-05-15 05:29:23 -0300

    import não utilizado removido

--- src/main/java/com/fatec/LBEGerenciadorDeProjetoSimples/controller/SobreNosController.java
+++ src/main/java/com/fatec/LBEGerenciadorDeProjetoSimples/controller/SobreNosController.java
@@ -2,7 +2,6 @@ package com.fatec.LBEGerenciadorDeProjetoSimples.controller;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.fatec.LBEGerenciadorDeProjetoSimples.model.Login;
-import com.fatec.LBEGerenciadorDeProjetoSimples.repository.ILoginRepository;
-
 @Controller
 public class SobreNosController {
-	
-	@Autowired
-	private ILoginRepository cadastroRep;
 	
 	@RequestMapping(name = "sobre-nos", value = "/sobre-nos", method = RequestMethod.GET)
 	public ModelAndView projetoGet(ModelMap model) {
--- src/main/java/com/fatec/LBEGerenciadorDeProjetoSimples/model/Equipe.java
+++ src/main/java/com/fatec/LBEGerenciadorDeProjetoSimples/model/Equipe.java
@@ -1,14 +1,12 @@
 package com.fatec.LBEGerenciadorDeProjetoSimples.model;
 
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.NamedNativeQuery;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
